4b12c8251f1a7d99b91c2da80e62375f10667d53
Gyeo1 <rlatjdruf99@naver.com>
2021-10-23 12:51:11 +0900

    정렬 예제 문제 풀이

--- 21_Fall/6.정렬/예제3)퀵 정렬.py
+++ 21_Fall/6.정렬/예제3)퀵 정렬.py
@@ -13,3 +13,32 @@ def quick_sort(array):
 
     return quick_sort(left)+[pivot]+quick_sort(rigth) #재귀 호출로 left와 rigth 사이드를 다시 quick sort시켜준다,
 print(quick_sort(array))
+
+
+l_side=[x for x in range(10) if x%2==0]
+
+#정석 풀이
+# array=[5,7,9,0,3,1,6,2,4,8]
+#
+# def quick(array,start, end):
+#     if start>=end: #원소가 하나만 있는 경우?
+#         return
+#     #인덱스
+#     pivot=start
+#     left=start+1
+#     rigth=end
+#     while left<=rigth: #교차될때 까지?
+#         while left<=end and array[left] <=array[pivot] :
+#             left+=1 #인덱스를 계속 증가 시켜줌 언제까지? ==>pivot보다 작은 값이 나올때 까지.
+#         while rigth>start and array[rigth] >=array[pivot]:
+#             rigth-=1
+#         if left>rigth:# 엇갈렸다면 피벗 교체
+#             array[rigth], array[pivot] = array[pivot], array[rigth]
+#         else:
+#             array[left], array[rigth]=array[rigth], array[left]
+#
+#     #교차가 되면 재귀 함수로 다시 호출!.
+#     quick(array,start, rigth-1)
+#     quick(array, rigth+1, end)
+# quick(array,0,len(array)-1)
+# print(array)
