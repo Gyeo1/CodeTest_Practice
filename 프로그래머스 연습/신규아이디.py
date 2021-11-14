a328300b969e94b91a5c91390b037b1eb307ca9f
Gyeo1 <rlatjdruf99@naver.com>
2021-11-15 01:01:44 +0900

    123

--- 프로그래머스 연습/신규아이디.py
+++ 프로그래머스 연습/신규아이디.py
@@ -2,11 +2,15 @@ import sys
 
 def solution(new_id):
 
+    #Step1 대문자 소문자로 바꿔주기
     for i in range(len(new_id)):
         a=ord(new_id[i])
-        if a>=65 and a<=90: #대문자 ?
+        if a>=65 and a<=90:
             a+=32
             new_id[i]=chr(a)
+    #Step2 알파벳 소문자 숫자 - _ . 제외하고 모두 제거
+    for j in range(len(new_id)):
+        a = ord(new_id[j])
 
     return str(new_id)
 
