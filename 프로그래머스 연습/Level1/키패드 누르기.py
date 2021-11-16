41721c1f4fbfd5fceb365af12524de0b1afa20fa
Gyeo1 <rlatjdruf99@naver.com>
2021-11-16 13:14:34 +0900

    no message

--- 프로그래머스 연습/Level1/키패드 누르기.py
+++ 프로그래머스 연습/Level1/키패드 누르기.py
@@ -33,10 +33,3 @@ def solution(numbers, hand):
 numbers=[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
 hand="right"
 print(solution(numbers,hand))
-# key_pad={1:[0,0], 2:[0,1], 3:[0,2], 4:[1,0], 5:[1,1], 6:[1,2],7:[2,0], 8:[2,1], 9:[2,2],
-#              "star":[3,0],0:[3,1],"sharp":[3,2]}
-# left=key_pad["star"]
-# right=key_pad["sharp"]
-# distance1=abs(key_pad[3][0]-left[0])+abs(key_pad[3][1]-left[1]) #키패드간 좌표거리
-# print(distance1)
-LRLLLRLLRRL
