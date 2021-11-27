b8869083e903dcede10ad7362352f5427716179f
Gyeo1 <rlatjdruf99@naver.com>
2021-11-27 13:15:15 +0900

    no message

--- 프로그래머스 연습/Level2/타겟넘버.py
+++ 프로그래머스 연습/Level2/타겟넘버.py
@@ -1,59 +1,68 @@
 # def solution(numbers, target):
 #     answer = 0
 #     numbers.sort(reverse=True)
-#     count = 0
-#     def recursive(check,current,target):
-#         nonlocal count
-#         check[current] = -(check[current])
-#         if sum(check)==target:
+#     count=0
+#
+#     if sum(numbers)==target:
+#         answer=1
+#         return  answer
+#     def recursive(current,index,target):
+#         print(current)
+#         count=0
+#         if index >len(current)-1:
+#             return count
+#         if sum(current)==target:
 #             count+=1
 #             return count
-#         if sum(check)<target and current<len(check)-1:
-#             check[current] = -(check[current])
-#             current += 1    #다음 인덱스로?
-#             recursive(check,current,target)
-#         if sum(check)>target and current<len(check)-1:
-#             current+=1
-#             recursive(check,current,target)
+#         elif sum(current)<target:
+#             current[index] = (-current[index]) #현재 인덱스는 +로 바꿔주고 다음 인덱스를 -로
+#             index += 1
+#             if index < len(current) - 1:
+#                 current[index] = (-current[index])
+#                 count += recursive(current, index, target)
+#         else: #current>target
+#             index+=1
+#             if index <= len(current) - 1:
+#                 current[index]=(-current[index])
+#                 count += recursive(current, index, target)
+#
 #         return count
+#
+#
 #     for i in range(len(numbers)):
-#         check=numbers[:]
-#         count=0
-#         if sum(check)==target:
-#             answer+=1
-#             break
-#         elif sum(check)<target:
-#             break
-#         answer+=recursive(check,i,target)
+#         numbers[i] = (-numbers[i])
+#         b=numbers[:]
+#
+#         if sum(numbers)>=target:
+#             print(i,"번째 입니다")
+#             answer+=recursive(b,i,target)
+#             print(answer)
+#         numbers[i] = (-numbers[i])
+#
 #     return answer
-# def solution(numbers, target):
-#     if numbers == []:
-#         if target == 0:
-#             return 1
-#         else:
-#             return 0
-#     else:
-#         return solution(numbers[1:], target+numbers[0]) + solution(numbers[1:], target-numbers[0])
-from collections import deque
-
+#
+# test case 만 통과 ;
 def solution(numbers, target):
+
     answer = 0
-    queue = deque([(0, 0)]) # sum, level
-    while queue:
-        s, l = queue.popleft()
-        print(queue)
-        if l > len(numbers):
-            break
-        elif l == len(numbers) and s == target:
+    N = len(numbers)
+
+    def dfs(idx, numbers, total):
+        nonlocal answer
+        if (idx == N) and (total == target):
             answer += 1
-        queue.append((s+numbers[l-1], l+1))
-        queue.append((s-numbers[l-1], l+1))
+            return
+        if idx == N:
+            return
+
+        dfs(idx + 1, numbers, total + numbers[idx])
+        dfs(idx + 1, numbers, total - numbers[idx])
+
+    dfs(0, numbers, 0)
 
     return answer
-numbers=[1,1,4,3,2]
-print(numbers[-1])
-target=3
+numbers=[1, 1, 1, 1, 1]
+target=5
 print(solution(numbers,target))
-#숫자를 적절히 더하고 빼서 타겟 넘버를 완성하세요
-#역수로 뒤집고 일단 다 더해봐 더한 값이 target보다 작으면 return 0
-#더한 값이 target보다 크면 number[0]부터 -로 바꿔주고 다시 실행해 본다.
+#n개의 음이 아닌 정수가 있다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들어라
+#sort 해주고
