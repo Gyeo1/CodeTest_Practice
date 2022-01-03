64e0bb7af96ddba4bc35babca9dcb32f6bd7cb65
Gyeo1 <rlatjdruf99@naver.com>
2022-01-03 21:02:41 +0900

    큰수

--- 프로그래머스 연습/Level2/큰수 만들기(그리디).py
+++ 프로그래머스 연습/Level2/큰수 만들기(그리디).py
@@ -5,12 +5,19 @@ def solution(number, k):
     th = -1
     for i in range(lenResult):  #결과값의 최대 길이만큼 반복
         val = "0"
+        #lenNumber-(lenResult-len(results))+1 여기 부분이 핵심, lenResult는 내가 구해야되는 숫자의 자릿수이고 result의 자릿수만큼 빼주는거다
         for j in range(th+1, lenNumber -(lenResult - len(results))+1):  #th부터 전체 길이-(결과로 나와야되는 길이-result 길이)
 
+            print(j,end=" ")
             if val < number[j]:     #val은 맨 처음에는 0, 이후에는 큰값이 들어간다
                 th = j              #th에 인덱스 값 추가, 이후 인덱스 부터 실행한다.
                 val = number[j]
                 if val == "9": break
+
         results.append(val)        #result에 가장 큰 값을 넣어준다.
+        print(results,val, th)
     answer = "".join(results)
     return answer
+number="4177252841"
+k=4
+print(solution(number,k))
