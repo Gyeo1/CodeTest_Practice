ffc9c12adb55ff8acd494d000ecf2bc7456a9399
Gyeo1 <rlatjdruf99@naver.com>
2022-02-16 23:48:52 +0900

    양궁대회 풀이중

--- 프로그래머스 연습/Level2/양궁대회(2022카카오).py
+++ 프로그래머스 연습/Level2/양궁대회(2022카카오).py
@@ -2,55 +2,54 @@
 
 #k점을 더 많이 맞춘 사람이 해당 점수를 가져간다. 대신 동일 횟수로 맞추면 어피치에게 점수를 주는 패널티!
 #추가 패널티는 점수가 같아도 어피치가 우승
+
 max_val=0
-answer=[]
-def check_win(l1,l2):
-    global max_val,answer
-    #l1은 어피치 l2는 라이언 점수 값임
-    apeach=0
-    lion=0
-    for i in range(len(l1)):
-        if l1[i]<l2[i]:
+abc=[]
 
-            lion+=(10-i)
-        elif l1[i]>l2[i]:
-            apeach+=(10-i)
-    print(l1, l2)
-    print(apeach, lion)
-    # if max_val<lion-apeach: #최대 점수차 갱신?
-    #     answer=l2
-    #     print(answer)
-    #     max_val=lion-apeach
-
-    if apeach<lion:
-        return True
-    return False
 
 def solution(n, info):
-    if n ==1 and info[0]==1:
-        return [-1]
+    global abc
+    answer = [-1]
+
     for i in range(len(info)):
-        check=[0 for _ in range(len(info))]
-        for j in range(i,len(info)):
-            if sum(check)+info[j]+1<=n:     #현재 lion이 쏜 횟수 + 어피치를 이기기 위해 쏜 횟수가 n보다 작으면 부여
-                check[j]=info[j]+1
-            # print(check)
-            # if sum(check)>=n or :
-            # if check_win(info,check): #이게 True면? 라이언 점수가 더 큼 그럼 j 기준 다음의 값으로 확인
-            #     print(check)
-            while check_win(info,check):
-                check[j]=0
-                j+=1
-                if sum(check) + info[j] + 1 <= n:  # 현재 lion이 쏜 횟수 + 어피치를 이기기 위해 쏜 횟수가 n보다 작으면 부여
+        check = [0 for _ in range(len(info))]
+        for j in range(i, len(info)):
+            if sum(check) + info[j] + 1 <= n:
+                check[j] = info[j] + 1
+
+            elif check_win(info, check):
+                answer=check[:]
+
+                check[j - 1] = 0
+                if sum(check) + info[j] + 1 <= n:
                     check[j] = info[j] + 1
+        # print(check)
+    return answer
+
+def check_win(l1,l2):
+    global max_val,abc
+    apeach = 0
+    lion = 0
+    for i in range(len(info)):
+        if l1[i] < l2[i]:
+            lion += (10 - i)
+        elif l1[i] > l2[i]:
+            apeach += (10 - i)
+
+    if apeach < lion:
+        if max_val < lion - apeach:  # 최대 점수차 갱신?
+            max_val = lion - apeach
+            return True
+    #l1은 어피치 l2는 라이언 점수 값임
+    return False
 
 
 
-n=9
-info=[0,0,1,2,0,1,1,1,1,1,1]#10점부터 0점까지 맞춘횟수임.
-solution(n,info)
+n=5
+info=[2,1,1,1,0,0,0,0,0,0,0]	#10점부터 0점까지 맞춘횟수임.
+print(solution(n,info))
 
-print(max_val)
+
 
 #n은 화살의 갯수
 #info는 어피치가 맞춘 과녁의 점수임
