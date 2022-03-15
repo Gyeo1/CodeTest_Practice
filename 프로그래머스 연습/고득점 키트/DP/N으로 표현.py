900c392d07a6be98475a186536f8a0e4f7af6c7e
Gyeo1 <rlatjdruf99@naver.com>
2022-03-15 23:30:24 +0900

    no message

---
+++ 프로그래머스 연습/고득점 키트/DP/N으로 표현.py
@@ -0,0 +1,15 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42895 레벨3 문제
+
+#Question ==> N과 사칙연산만으로 number를 표현할 수 있다. 표현할때 N의 사용횟수가 가장 적은 값을 return 하도록 하시오
+
+def solution(N, number):
+    if N==number:
+        return 1
+    answer = 0
+
+    return answer
+
+N=5
+number=12
+
+print(solution(N,number))
